8579a6ddb390ad7ba1fa257f7d92c17eeb95f176
anilyumukk <Anil.yumuk@hotmail.com>
2023-02-17 17:35:26 +0300

    17.02.23

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,18 +1,18 @@
 export interface User {
-    id:number,
-    name: string,
-    lastname:string,
-    UserName:string,
-    password:string,
-    jira:number,
-    birthDate:Date,
-    telno:string,
-    email:string,
-    photo:string,
-    permit:number,
-    userid:number,
-    USER_ID:string
-    firsttime:Date,
-    lasttime:Date
-
+  id: number;
+  name: string;
+  lastname: string;
+  UserName: string;
+  password: string;
+  jira: number;
+  birthDate: Date;
+  telno: string;
+  email: string;
+  photo: string;
+  permit: number;
+  userid: number;
+  USER_ID: string;
+  firsttime: Date;
+  lasttime: Date;
+  pdks: any;
 }
